ee428969d172f61ff0096cc7a8e8a66da8c19b19
hrmrakib <hrmrakibs@gmail.com>
2025-01-07 19:54:39 +0600

    first commit

---
+++ output/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function func(a, b) {
+    console.log(a + b);
+}
+func(10, 15);
+/**
+ * npm i -g typescript
+ * npm init -y
+ * tsc --init
+ * tyconfig.json (modify necessary rules)
+ */
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+function func(a: number, b: number) {
+  console.log(a + b);
+}
+
+func(10, 15);
+
+/**
+ * npm i -g typescript
+ * npm init -y
+ * tsc --init
+ * tyconfig.json (modify necessary rules)
+ */
