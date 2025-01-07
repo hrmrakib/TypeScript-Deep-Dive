212197f371b68a894593a76d78472668040873e2
hrmrakib <hrmrakibs@gmail.com>
2025-01-07 20:52:46 +0600

    special type in ts

---
+++ output/special-type.js
@@ -0,0 +1,22 @@
+"use strict";
+// Type: any
+// any is a type that disables type checking and effectively allows all types to be used.
+let fruit = "Apple";
+fruit = 101;
+fruit = false;
+console.log(fruit);
+// Type: unknown
+/* unknown is a similar, but safer alternative to any. */
+let animal = "Cow";
+animal = true;
+animal = null;
+console.log(animal);
+// Type: never
+/* never effectively throws an error whenever it is defined. */
+let opps;
+console.log(opps);
+// Type: undefined & null
+/* undefined and null are types that refer to the JavaScript primitives undefined and null respectively. */
+let what = undefined;
+let why = null;
+console.log(what, why);
---
+++ src/special-type.ts
@@ -0,0 +1,24 @@
+// Type: any
+// any is a type that disables type checking and effectively allows all types to be used.
+let fruit: any = "Apple";
+fruit = 101;
+fruit = false;
+console.log(fruit);
+
+// Type: unknown
+/* unknown is a similar, but safer alternative to any. */
+let animal: unknown = "Cow";
+animal = true;
+animal = null;
+console.log(animal);
+
+// Type: never
+/* never effectively throws an error whenever it is defined. */
+let opps: never;
+console.log(opps);
+
+// Type: undefined & null
+/* undefined and null are types that refer to the JavaScript primitives undefined and null respectively. */
+let what: undefined = undefined;
+let why: null = null;
+console.log(what, why);
