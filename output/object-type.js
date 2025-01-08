c24f8992ccc4daebf2d146fd018668a8d7732383
hrmrakib <hrmrakibs@gmail.com>
2025-01-08 07:10:47 +0600

    type aliases in ts object

--- output/object-type.js
+++ output/object-type.js
@@ -1,4 +1,5 @@
 "use strict";
+// Inline Type Annotation: Directly specify the object's structure during declaration.
 const student = {
     id: 10001,
     name: "Abu Nayem",
@@ -9,3 +10,13 @@ const student = {
     },
 };
 console.log(student);
+const car = {
+    type: "private car",
+    model: "BMW",
+    year: 2020,
+};
+// Index Signatures
+// Index signatures can be used for objects without a defined list of properties.
+const person = {};
+person.id = 101;
+person.name = "Mofiz";
--- src/object-type.ts
+++ src/object-type.ts
@@ -1,3 +1,4 @@
+// Inline Type Annotation: Directly specify the object's structure during declaration.
 const student: {
   id: number;
   name: string;
@@ -14,3 +15,22 @@ const student: {
 };
 
 console.log(student);
+
+// Type Aliases: Create a reusable type definition.
+type TCar = {
+  type: string;
+  model: string;
+  year: number;
+};
+
+const car: TCar = {
+  type: "private car",
+  model: "BMW",
+  year: 2020,
+};
+
+// Index Signatures
+// Index signatures can be used for objects without a defined list of properties.
+const person: { [index: string]: number | string } = {};
+person.id = 101;
+person.name = "Mofiz";
