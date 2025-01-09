b1cdd5bbbe913dc23d1bc02c18ba7237f31b4bf4
hrmrakib <hrmrakibs@gmail.com>
2025-01-09 09:54:30 +0600

    default parameter

--- output/functions.js
+++ output/functions.js
@@ -1,5 +1,11 @@
 "use strict";
+// Basic Function with Optional Paramters
 function addition(a, b, c) {
     return a + b + (c || 0);
 }
 console.log(addition(10, 20));
+// Default paramter
+const point = (n) => {
+    return n * n;
+};
+console.log(point(3));
--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,13 @@
+// Basic Function with Optional Paramters
 function addition(a: number, b: number, c?: number): number {
   return a + b + (c || 0);
 }
 
 console.log(addition(10, 20));
+
+// Default paramter
+const point = (n: number): number => {
+  return n * n;
+};
+
+console.log(point(3));
